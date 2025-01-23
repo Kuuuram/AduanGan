2e37a84399ef4cc8618185136664920bc5937fc6
Kuuuram <kuramamiplay@gmail.com>
2025-01-23 13:49:05 +0700

    Isi

---
+++ Werbb.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("Halo, kami dari kelompok 5 :wave:")
+st.title("Kelompok 5")
+st.write("Ada Andra, Iwa, Gunawan, dan Our Beloved Nickles")
